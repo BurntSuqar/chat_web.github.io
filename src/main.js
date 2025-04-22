d5670fa2040b8d47f6ab2d297b53820b8aec3560
BurntSuqar <13148754080@163.com>
2025-04-22 19:17:38 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import './style.scss'
+import App from './App.vue'
+import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import "../public/config.js";
+
+const app = createApp(App)
+app.use(router).use(ElementPlus).mount('#app')
+
+/* 处理错误 */
+app.config.errorHandler = (err) => {}
